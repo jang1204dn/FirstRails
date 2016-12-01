ffaa7e86559ef78f8143260ccba83ebb13953070
An Giang <vinhaz1204@gmail.com>
2016-12-01 18:48:38 +0700

    email set for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #mailling
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sengrid.net",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
+  # Actionmailer Config
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 end
